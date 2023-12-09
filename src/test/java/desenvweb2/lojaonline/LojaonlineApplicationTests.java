c43f1acbd3cf55c0b61b5a22e8019c6314b7e0eb
Uriel <urielrlps@gmail.com>
2023-12-09 13:40:33 -0300

    Initial commit

---
+++ src/test/java/desenvweb2/lojaonline/LojaonlineApplicationTests.java
@@ -0,0 +1,13 @@
+package desenvweb2.lojaonline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaonlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
