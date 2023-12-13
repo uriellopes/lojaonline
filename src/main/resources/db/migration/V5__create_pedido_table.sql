9abe4a6c4bcf66a4da410b92349cc35a10f21b2c
Uriel <uriel.lira@smartsuite.imd.ufrn.br>
2023-12-13 14:27:52 -0300

    Remover arquivos de migração

--- src/main/resources/db/migration/V5__create_pedido_table.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS pedido (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    id_usuario INT NOT NULL,
-    id_produto INT NOT NULL,
-    FOREIGN KEY (id_usuario) REFERENCES usuario(id),
-    FOREIGN KEY (id_produto) REFERENCES produto(id)
-);
